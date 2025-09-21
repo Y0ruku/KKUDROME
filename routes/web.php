1bd28c59334957ce33360858b12ad67ce5ddc3f6
Y0ruku <aaaa0651143374@gmail.com>
2025-09-21 22:10:58 +0700

    chang

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
-
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
